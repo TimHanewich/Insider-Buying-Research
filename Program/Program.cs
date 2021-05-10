dabd74cfc21f7068417a270b0887e57ff7cb7a84
Tim Hanewich <tahanewich@live.com>
2021-05-09 21:05:40 -0400

    Initial commit

---
+++ Program/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Insider_Buying_Research
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
