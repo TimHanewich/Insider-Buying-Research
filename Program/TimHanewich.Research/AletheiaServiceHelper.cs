e84a2a9fffb2cb3500c7ef6856d02217742e94f1
Tim Hanewich <tahanewich@live.com>
2021-05-09 22:14:21 -0400

    Made AletheiaServiceHelper

---
+++ Program/TimHanewich.Research/AletheiaServiceHelper.cs
@@ -0,0 +1,71 @@
+using System;
+using Aletheia.Service;
+using Aletheia.Service.InsiderTrading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Aletheia.InsiderTrading;
+using SecuritiesExchangeCommission.Edgar;
+namespace TimHanewich.Reserch
+{
+    public class AletheiaServiceHelper
+    {
+        public event StatusUpdate StatusChanged;
+        private string AletheiaApiKey;
+
+        public AletheiaServiceHelper(string api_key)
+        {
+            AletheiaApiKey = api_key;
+        }
+
+        public async Task<SecurityTransactionHolding[]> GetAllSecurityTransactionHoldingsAsync(string company_cik_or_symbol)
+        {
+            AletheiaService service = new AletheiaService(AletheiaApiKey);
+            UpdateStatus("Getting starter transactions...");
+            SecurityTransactionHolding[] sths = await service.LatestTransactionsAsync(company_cik_or_symbol, null, 100, null, SecurityType.NonDerivative, TransactionType.OpenMarketOrPrivatePurchase);
+
+            //Collect
+            List<SecurityTransactionHolding> ToReturn = new List<SecurityTransactionHolding>();
+            while (sths.Length > 0)
+            {
+                UpdateStatus("Adding " + sths.Length.ToString() + " transactions...");
+
+                foreach (SecurityTransactionHolding sth in sths)
+                {
+                    ToReturn.Add(sth);
+                }
+                
+                //Find the oldest transaction in the group
+                UpdateStatus("Finding oldest transaction...");
+                DateTime oldest = DateTime.Now;
+                foreach (SecurityTransactionHolding sth in sths)
+                {
+                    if (sth.TransactionDate.HasValue)
+                    {
+                        if (sth.TransactionDate < oldest)
+                        {
+                            oldest = sth.TransactionDate.Value;
+                        }
+                    }
+                }
+                UpdateStatus("Oldest transaction in this group: " + oldest.ToShortDateString());
+
+                //Search again
+                UpdateStatus("Querying again...");
+                sths = await service.LatestTransactionsAsync(company_cik_or_symbol, null, 100, null, SecurityType.NonDerivative, TransactionType.OpenMarketOrPrivatePurchase);
+            }
+
+            UpdateStatus("Collection complete! " + ToReturn.Count.ToString("#,##0") + " found.");
+            return ToReturn.ToArray();
+        }
+
+        private void UpdateStatus(string msg)
+        {
+            if (StatusChanged != null)
+            {
+                StatusChanged.Invoke(msg);
+            }
+        }
+    }
+    
+    public delegate void StatusUpdate(string msg);
+}
