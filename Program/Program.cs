811a028a1fdc6092330e99d4f73a4c14fed1f995
Tim Hanewich <tahanewich@live.com>
2021-05-11 21:52:54 -0400

    Removed the ability to download filings

--- Program/Program.cs
+++ Program/Program.cs
@@ -10,24 +10,8 @@ namespace Insider_Buying_Research
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What would you like to do?");
-            Console.WriteLine("1 - Download transactions");
-            Console.WriteLine("2 - Download filings");
-            Console.Write(">");
-            string cmd = Console.ReadLine();
             SecCollectionHelper colhelp = new SecCollectionHelper();
-            if (cmd == "1")
-            {
-                colhelp.StoreSP500NonDerivateTransactionsInFolderAsync(args[0]).Wait();
-            }
-            else if (cmd == "2")
-            {
-                colhelp.StoreSP500StatementOfOwnershipsInFolderAsync(args[0]).Wait();
-            }
-            else
-            {
-                Console.WriteLine("Not recognized");
-            }  
+            colhelp.StoreSP500NonDerivateTransactionsInFolderAsync(args[0]).Wait();
         }
     }
 }
--- Program/TimHanewich.Research/SecCollectionHelper.cs
+++ Program/TimHanewich.Research/SecCollectionHelper.cs
@@ -11,68 +11,6 @@ namespace TimHanewich.Reserch
     {
         private bool PrintStatus;
 
-        public async Task<StatementOfBeneficialOwnership[]> GetAllStatementOfBeneficialOwnershipsAsync(string symbol_or_cik, bool print_status)
-        {
-            List<StatementOfBeneficialOwnership> ToReturn = new List<StatementOfBeneficialOwnership>();
-            PrintStatus = print_status;
-
-            TryPrintStatus("Getting all form 3 filings...");
-            EdgarFiling[] filings3 = await AletheiaToolkit.CollectAllFilingsOfTypeAsync(symbol_or_cik, "3", false);
-            TryPrintStatus(filings3.Length.ToString("#,##0") + " form 3's found");
-            TryPrintStatus("Getting all form 4 filings...");
-            EdgarFiling[] filings4 = await AletheiaToolkit.CollectAllFilingsOfTypeAsync(symbol_or_cik, "4", false);
-            TryPrintStatus(filings4.Length.ToString("#,##0") + " form 4's found");
-            TryPrintStatus("Getting all form 5 filings...");
-            EdgarFiling[] filings5 = await AletheiaToolkit.CollectAllFilingsOfTypeAsync(symbol_or_cik, "5", false);
-            TryPrintStatus(filings5.Length.ToString("#,##0") + " form 5's found");
-
-            //Add to a basket
-            List<EdgarFiling> AllFilings = new List<EdgarFiling>();
-            AllFilings.AddRange(filings3);
-            AllFilings.AddRange(filings4);
-            AllFilings.AddRange(filings5);
-            TryPrintStatus("Total of " + AllFilings.Count.ToString("#,##0") + " found!");
-
-            //Convert all of these
-            int counter = 1;
-            foreach (EdgarFiling ef in AllFilings)
-            {
-                string prefix = "# " + counter.ToString("#,##0") + " / " + AllFilings.Count.ToString("#,##0") + ": ";
-                TryPrintStatus("Starting collection process for filing # " + counter.ToString("#,##0") + " / " + AllFilings.Count.ToString("#,##0"));
-
-                TryPrintStatus(prefix + "Getting filing details");
-                EdgarFilingDetails efd = await ef.GetFilingDetailsAsync();
-
-                //Finding file
-                TryPrintStatus(prefix + "Finding data file.");
-                foreach (FilingDocument fd in efd.DocumentFormatFiles)
-                {
-                    if (fd.DocumentType == "4")
-                    {
-                        if (fd.DocumentName.ToLower().Contains(".xml"))
-                        {
-                            try
-                            {
-                                //Process the document
-                                TryPrintStatus(prefix + "Downloading and parsing data file.");
-                                StatementOfBeneficialOwnership thisform = await StatementOfBeneficialOwnership.ParseXmlFromWebUrlAsync(fd.Url);
-                                ToReturn.Add(thisform);
-                            }
-                            catch
-                            {
-                                TryPrintStatus(prefix + "FAILURE! Moving on......");
-                            }
-                        }
-                    }
-                }
-                
-                counter = counter + 1;
-            }
-            
-            TryPrintStatus("Complete! " + ToReturn.Count.ToString("#,##0") + " ownership forms transactions found.");
-            return ToReturn.ToArray();
-        }
-
         public async Task<NonDerivativeTransaction[]> GetAllNonDerivativeTransactionsAsync(string symbol_or_cik, bool print_status = false)
         {
             List<NonDerivativeTransaction> ToReturn = new List<NonDerivativeTransaction>();
@@ -184,63 +122,6 @@ namespace TimHanewich.Reserch
                         NonDerivativeTransaction[] transactions = await GetAllNonDerivativeTransactionsAsync(s, true);
                         Console.WriteLine("Writing to file...");
                         System.IO.File.WriteAllText(folder_path + "\\" + s + ".json", JsonConvert.SerializeObject(transactions));
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("Fatal failre on stock " + s + ": " + ex.Message);
-                        Console.ForegroundColor = oc;
-                    }
-                }
-                            
-            }
-        }
-
-        public async Task StoreSP500StatementOfOwnershipsInFolderAsync(string folder_path)
-        {
-            Console.WriteLine("Getting S&P500 list...");
-            string[] sp500 = await TimHanewich.Investing.InvestingToolkit.GetEquityGroupAsync(EquityGroup.SP500);
-            Console.WriteLine(sp500.Length.ToString() + " stocks found.");
-
-            foreach (string s in sp500)
-            {
-                ConsoleColor oc = Console.ForegroundColor;
-                Console.ForegroundColor = ConsoleColor.Blue;
-                Console.WriteLine("[ " + s.ToString() + " ]");
-                Console.ForegroundColor = oc;
-
-                //Check if there is a file with this already
-                Console.WriteLine("Checking if should collect this one.");
-                bool ShouldCollect = true;
-                if (System.IO.File.Exists(folder_path + "\\" + s.ToUpper().Trim() + ".json"))
-                {
-                    Console.WriteLine("File already exists. Checking if it has content.");
-                    //Try to pick it up and see how many are in there
-                    StatementOfBeneficialOwnership[] forms = JsonConvert.DeserializeObject<StatementOfBeneficialOwnership[]>(System.IO.File.ReadAllText(folder_path + "\\" + s.ToUpper().Trim() + ".json"));
-                    if (forms.Length == 0)
-                    {
-                        Console.WriteLine("It does not have content. Collecting!");
-                        ShouldCollect = true;
-                    }
-                    else
-                    {
-                        Console.WriteLine("It does have " + forms.Length.ToString("#,##0") + " forms, skipping this one!");
-                        ShouldCollect = false;
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("File does not already exist. Will collect!");
-                }
-
-                if (ShouldCollect)
-                {
-                    try
-                    {
-                        Console.WriteLine("Collecting...");
-                        StatementOfBeneficialOwnership[] forms = await GetAllStatementOfBeneficialOwnershipsAsync(s, true);
-                        Console.WriteLine("Writing to file...");
-                        System.IO.File.WriteAllText(folder_path + "\\" + s + ".json", JsonConvert.SerializeObject(forms));
                     }
                     catch (Exception ex)
                     {
