2ee0e43549d6667b40339ab3d4ec149c2c7b4679
Tim Hanewich <tahanewich@live.com>
2021-05-11 21:38:33 -0400

    Program can get transactions or filings

--- Program/Program.cs
+++ Program/Program.cs
@@ -10,8 +10,24 @@ namespace Insider_Buying_Research
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What would you like to do?");
+            Console.WriteLine("1 - Download transactions");
+            Console.WriteLine("2 - Download filings");
+            Console.Write(">");
+            string cmd = Console.ReadLine();
             SecCollectionHelper colhelp = new SecCollectionHelper();
-            colhelp.StoreSP500StatementOfOwnershipsInFolderAsync(args[0]).Wait();
+            if (cmd == "1")
+            {
+                colhelp.StoreSP500NonDerivateTransactionsInFolderAsync(args[0]).Wait();
+            }
+            else if (cmd == "2")
+            {
+                colhelp.StoreSP500StatementOfOwnershipsInFolderAsync(args[0]).Wait();
+            }
+            else
+            {
+                Console.WriteLine("Not recognized");
+            }  
         }
     }
 }
