c960eaa95d96b73f6aebf47e597be6d323e27c29
Tim Hanewich <tahanewich@live.com>
2021-05-12 22:43:17 -0400

    Added full research set

---
+++ Program/TimHanewich.Research/Core/FullResearchSet.cs
@@ -0,0 +1,107 @@
+using System;
+using SecuritiesExchangeCommission.Edgar;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace TimHanewich.Reserch.Core
+{
+    public class FullResearchSet
+    {
+        //Settings
+        public bool PrintStatusChanges;
+
+        public string Symbol {get; set;}
+        public NonDerivativeTransaction[] AnalyzedTransactions {get; set;}
+        public StockPerformanceSet AveragePerformance {get; set;}
+        public StockPerformanceSet[] PerformancesFollowingInsiderBuys {get; set;}
+
+        public async Task GenerateFromTransactionsFileAsync(string path)    
+        {
+            //Start with vars
+            List<NonDerivativeTransaction> ta_AnalyzedTransactions = new List<NonDerivativeTransaction>();
+            List<StockPerformanceSet> ta_PerformancesFollowingInsiderBuys = new List<StockPerformanceSet>();
+
+            //Get the name of the stock
+            PrintStatus("Getting symbol from path.");
+            Symbol = System.IO.Path.GetFileName(path).Replace(".json", "").ToUpper().Trim();
+            Console.WriteLine("Symbol: " + Symbol);
+
+            //Get the transactions
+            ResearchToolkit rt = new ResearchToolkit();
+            PrintStatus("Loading transactions from file.");
+            NonDerivativeTransaction[] alltransactions = await rt.OpenNonDerivativeTransactionsAsync(path);
+            PrintStatus(alltransactions.Length.ToString("#,##0") + " loaded.");
+            PrintStatus("Filtering to only buys...");
+            NonDerivativeTransaction[] buys = rt.FilterToBuys(alltransactions);
+            PrintStatus(buys.Length.ToString() + " buys found.");
+            PrintStatus("Sorting...");
+            NonDerivativeTransaction[] sorted = rt.SortByTransactionDate(buys);
+            PrintStatus(sorted.Length.ToString() + " sorted!");
+            
+            //Filter to just the particular date range
+            //2011 - 2020, including 2011. So 10 years: 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020
+            PrintStatus("Filtering to only in range.");
+            List<NonDerivativeTransaction> InDateRange = new List<NonDerivativeTransaction>();
+            foreach (NonDerivativeTransaction ndt in sorted)
+            {
+                if (ndt.TransactionDate.HasValue)
+                {
+                    if (ndt.TransactionDate.Value >= new DateTime(2011, 1, 1)) //Older than (or equal to) 2011
+                    {
+                        if (ndt.TransactionDate.Value < new DateTime(2021, 1, 1)) //Before 2021
+                        {
+                            InDateRange.Add(ndt);
+                        }
+                    }
+                }
+            }
+            PrintStatus(InDateRange.Count.ToString() + " found in date range.");
+
+            //Check if there are any. If not, cancel
+            if (InDateRange.Count == 0)
+            {
+                PrintStatus("Nothing to analyze here! Cancelling.");
+                AnalyzedTransactions = ta_AnalyzedTransactions.ToArray();
+                AveragePerformance = null;
+                PerformancesFollowingInsiderBuys = ta_PerformancesFollowingInsiderBuys.ToArray();
+                return;
+            }
+
+            //Get the average performance
+            StockPerformanceSet sps_avg = new StockPerformanceSet();
+            PrintStatus("Getting average stock performance for this period.");
+            try
+            {
+                await sps_avg.CalculateAverageReturnsAsync(Symbol, new DateTime(2011, 1, 1), new DateTime(2020, 12, 31));
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Fatal failure while trying to download average returns for the period: " + ex.Message);
+            }
+            PrintStatus("Average perforamance downloaded!");
+            AveragePerformance = sps_avg;
+
+            //Get the performances after each buy
+            PrintStatus("Starting the performance calculation following each insider buy...");
+            foreach (NonDerivativeTransaction ndt in InDateRange)
+            {
+                StockPerformanceSet sps = new StockPerformanceSet();
+                PrintStatus("Calculating performance since buy on " + ndt.TransactionDate.Value.ToShortDateString() + "...");
+                await sps.CalculateReturnsAsync(Symbol, ndt.TransactionDate.Value);
+                Console.WriteLine("Performance calculated!");
+                ta_PerformancesFollowingInsiderBuys.Add(sps);
+            }
+
+            PrintStatus("Generation complete!");
+        }
+
+        private void PrintStatus(string s)
+        {
+            if(PrintStatusChanges)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
