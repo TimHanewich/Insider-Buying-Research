fd1f3900d576dcbbb41b1f8de4388bb718ad9efc
Tim Hanewich <tahanewich@live.com>
2021-05-12 20:55:37 -0400

    Finished stock performance calculator

--- Program/TimHanewich.Research/StockPerformanceCalculator.cs
+++ Program/TimHanewich.Research/StockPerformanceCalculator.cs
@@ -8,15 +8,38 @@ namespace TimHanewich.Reserch
     {
         public async Task<float> CalculateStockPerformanceAsync(string symbol, DateTime d1, DateTime d2)
         {
-            HistoricalDataProvider hdp1 = new HistoricalDataProvider();
-            await hdp1.DownloadHistoricalDataAsync(symbol, d1, d1); //Just for day 1
-            
-            HistoricalDataProvider hdp2 = new HistoricalDataProvider();
-            await hdp2.DownloadHistoricalDataAsync(symbol, d2, d2); //Just for day 2
-
-            float diff = hdp2.HistoricalData[0].AdjustedClose - hdp1.HistoricalData[0].AdjustedClose;
-            float aspercent = diff / hdp1.HistoricalData[0].AdjustedClose;
+            float p1 = await StockPriceClosestToDateAsync(symbol, d1);
+            float p2 = await StockPriceClosestToDateAsync(symbol, d2);
+            float diff = p2 - p1;
+            float aspercent = diff / p1;
             return aspercent;            
+        }
+
+        private async Task<float> StockPriceClosestToDateAsync(string symbol, DateTime target)
+        {
+            HistoricalDataProvider hdp = new HistoricalDataProvider();
+            await hdp.DownloadHistoricalDataAsync(symbol, target.AddDays(-3), target.AddDays(3));
+
+            //Check
+            if (hdp.HistoricalData.Length == 0)
+            {
+                throw new Exception("Unable to access historical stock data around date " + target.ToShortDateString());
+            }
+
+            //Find the closest
+            int closest_measure = int.MaxValue;
+            HistoricalDataRecord winner = hdp.HistoricalData[0];
+            foreach (HistoricalDataRecord hdr in hdp.HistoricalData)
+            {
+                TimeSpan ts = hdr.Date - target;
+                if (Math.Abs(ts.Days) < closest_measure)
+                {
+                    closest_measure = Math.Abs(ts.Days);
+                    winner = hdr;
+                }
+            }
+
+            return winner.AdjustedClose;
         }
     }
 }
