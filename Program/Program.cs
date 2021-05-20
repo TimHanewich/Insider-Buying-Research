982fbeb019d0a78d9fe49692eee9bdedc3cea630
Tim Hanewich <tahanewich@live.com>
2021-05-19 20:16:13 -0400

    Added check data method

--- Program/Program.cs
+++ Program/Program.cs
@@ -13,6 +13,94 @@ namespace Insider_Buying_Research
     {
         static void Main(string[] args)
         {
+            CheckData();
+        }
+
+        
+        //Step 1
+        public static void PerformFulAnalysis(string non_derivative_transactions_folder, string place_analyses_in)
+        {
+            string[] files = System.IO.Directory.GetFiles(non_derivative_transactions_folder);
+            foreach (string s in files)
+            {
+                string file_name = System.IO.Path.GetFileName(s);
+                AdminPrint("Checking " + file_name + "...");
+                if (System.IO.File.Exists(place_analyses_in + "\\" + file_name) == false)
+                {
+                    AdminPrint("This has not been research yet. Going!");
+
+                    //Reseaerch
+                    bool ResearchFailed = false;
+                    FullResearchSet frs = new FullResearchSet();
+                    frs.StatusPrintingOn();
+                    try
+                    {
+                        frs.GenerateFromTransactionsFileAsync(s).Wait();
+                    }
+                    catch (Exception ex)
+                    {
+                        AdminPrint("Generation for " + file_name + " failed. Msg: " + ex.Message, ConsoleColor.Red);
+                        ResearchFailed = true;
+                    }
+                    
+                    //Write it (if it has analyses)
+                    if (frs.PerformancesFollowingInsiderBuys != null)
+                    {
+                        if (frs.PerformancesFollowingInsiderBuys.Length > 0)
+                        {
+                            if (ResearchFailed == false)
+                            {
+                                AdminPrint("Writing to file...");
+                                System.IO.File.WriteAllText(place_analyses_in + "\\" + file_name, JsonConvert.SerializeObject(frs));
+                                AdminPrint("Successfully written!");
+                            }  
+                        }
+                        else
+                        {
+                            AdminPrint("Not going to write this one to file... It had no performance analyses following transctions!", ConsoleColor.Yellow);
+                        }
+                    }
+                    
+                }
+                else
+                {
+                    AdminPrint("This has already been researched. skipping.");
+                }
+            }
+        }
+    
+        //Step 2: Make sure all full analyses have data
+        public static void CheckData()
+        {
+            Console.Write("Path to the folder containing the analyses: ");
+            string path = Console.ReadLine();
+            path = path.Replace("\"", "");
+            string[] files = System.IO.Directory.GetFiles(path);
+            foreach (string s in files)
+            {
+                FullResearchSet frs = JsonConvert.DeserializeObject<FullResearchSet>(System.IO.File.ReadAllText(s));
+                bool flagthis = false;
+                if (frs.AveragePerformance == null)
+                {
+                    flagthis = true;
+                }
+                if (frs.PerformancesFollowingInsiderBuys == null)
+                {
+                    flagthis = true;
+                }
+                if (frs.PerformancesFollowingInsiderBuys.Length == 0)
+                {
+                    flagthis = true;
+                }
+                if (flagthis)
+                {
+                    Console.WriteLine(frs.Symbol);
+                }
+            }
+        }
+
+        public static void AssembleResultsCsv()
+        {
             Console.Write("Folder with the full analyses >");
             string analysis_folder = Console.ReadLine().Replace("\"", "");
 
@@ -77,57 +165,7 @@ namespace Insider_Buying_Research
             AdminPrint("Successfully wrote to " + path, ConsoleColor.Green);
         }
 
-        public static void PerformFulAnalysis(string non_derivative_transactions_folder, string place_analyses_in)
-        {
-            string[] files = System.IO.Directory.GetFiles(non_derivative_transactions_folder);
-            foreach (string s in files)
-            {
-                string file_name = System.IO.Path.GetFileName(s);
-                AdminPrint("Checking " + file_name + "...");
-                if (System.IO.File.Exists(place_analyses_in + "\\" + file_name) == false)
-                {
-                    AdminPrint("This has not been research yet. Going!");
 
-                    //Reseaerch
-                    bool ResearchFailed = false;
-                    FullResearchSet frs = new FullResearchSet();
-                    frs.StatusPrintingOn();
-                    try
-                    {
-                        frs.GenerateFromTransactionsFileAsync(s).Wait();
-                    }
-                    catch (Exception ex)
-                    {
-                        AdminPrint("Generation for " + file_name + " failed. Msg: " + ex.Message, ConsoleColor.Red);
-                        ResearchFailed = true;
-                    }
-                    
-                    //Write it (if it has analyses)
-                    if (frs.PerformancesFollowingInsiderBuys != null)
-                    {
-                        if (frs.PerformancesFollowingInsiderBuys.Length > 0)
-                        {
-                            if (ResearchFailed == false)
-                            {
-                                AdminPrint("Writing to file...");
-                                System.IO.File.WriteAllText(place_analyses_in + "\\" + file_name, JsonConvert.SerializeObject(frs));
-                                AdminPrint("Successfully written!");
-                            }  
-                        }
-                        else
-                        {
-                            AdminPrint("Not going to write this one to file... It had no performance analyses following transctions!", ConsoleColor.Yellow);
-                        }
-                    }
-                    
-                }
-                else
-                {
-                    AdminPrint("This has already been researched. skipping.");
-                }
-            }
-        }
-    
         private static void AdminPrint(string msg, ConsoleColor cc = ConsoleColor.Blue)
         {
             ConsoleColor oc = Console.ForegroundColor;
